0338fc61384ccf6c77dbb9ed5bc7716e46dc8515
Ariyabalan <159764671+Ariyabalan@users.noreply.github.com>
2024-04-04 23:18:12 +0530

    updated

---
+++ perfumy-react/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar"
+import About from "./components/About"
+import Product from "./components/Product"
+import Search from "./components/Search"
+import Footer from "./components/Footer"
+
+
+function App()
+{
+    return(
+        <div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+        </div>
+    )
+}
+export default App
---
+++ perfumy-react/src/components/Search.js
@@ -0,0 +1,10 @@
+//search component
+function Search()
+{
+  return(
+    <div className="searchbar">
+      <input type="text" placeholder="search"></input>
+    </div>
+  )
+}
+ export default Search
